8d29b4e217ea2bef6d3191f5f3947ecb478635b2
alfjjacob <alfjjacob@users.noreply.github.com>
2022-02-10 13:57:45 -0500

    Update setupProxy.js

--- client-reactjs/src/setupProxy.js
+++ client-reactjs/src/setupProxy.js
@@ -1,5 +1,6 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
+  console.log('PROXY_URL: '+ process.env.REACT_APP_PROXY_URL);
   app.use(createProxyMiddleware('/api', { target: process.env.REACT_APP_PROXY_URL }));
 };
