2aba21511ee8609e534abc919f7e53242888e9cc
alfjjacob <alfjjacob@users.noreply.github.com>
2022-02-22 15:54:28 -0500

    Update run_nginx.sh

--- client-reactjs/nginx/run_nginx.sh
+++ client-reactjs/nginx/run_nginx.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
+echo "start ssh server"
+(sleep 2;/usr/sbin/sshd -D -e)
+echo "started ssh server"
+
 echo "######## Run nginx"
 hostname="$1"
 export HOSTNAME=$hostname
 export DOLLAR='$'
 envsubst < /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/conf.d/tombolo-nginx.conf
 nginx -g "daemon off;"
-
-echo "start ssh server"
-/usr/sbin/sshd
